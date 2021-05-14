794f6f59b0e71edd625b50ce14589c717bf8999e
S.Ignatyuk <signatyuk89@gmail.com>
2021-05-14 16:56:14 +0300

    two dao methods finished

--- src/main/java/com/dao/jdbc/JdbcDepartmentDaoImpl.java
+++ src/main/java/com/dao/jdbc/JdbcDepartmentDaoImpl.java
@@ -2,28 +2,70 @@ package com.dao.jdbc;
 
 import com.dao.DepartmentDao;
 import com.model.Department;
+import com.model.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Repository
 public class JdbcDepartmentDaoImpl implements DepartmentDao {
 
     private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
-    private static final String SQL_SELECT_DEPARTMENTS = "SELECT id, name, " +
-            "description, phone_number, date_of_formation FROM departments";
+    private static final String SQL_SELECT_DEPARTMENTS = "SELECT d.id, d.name, " +
+            "d.description, d.phone_number, d.date_of_formation, e.id AS employee_id, " +
+            "e.full_name, e.date_of_birth, e.phone_number, e.email_address, " +
+            "e.position, e.date_of_employment FROM departments d " +
+            "LEFT JOIN employees e on d.id = e.department_id";
 
     @Autowired
     public JdbcDepartmentDaoImpl(DataSource dataSource) {
         this.namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
     }
 
+    private ResultSetExtractor<List<Department>> resultSetExtractor() {
+        return (rs) -> {
+            Map<Long, Department> map = new HashMap<>();
+            Department department;
+            while (rs.next()) {
+                Long id = rs.getLong("id");
+                department = map.get(id);
+                if (department == null) {
+                    department = Department.builder()
+                            .id(id)
+                            .name(rs.getString("name"))
+                            .description((rs.getString("description")))
+                            .phoneNumber(rs.getString("phone_number"))
+                            .dateOfFormation(rs.getDate("date_of_formation"))
+                            .build();
+                    map.put(id, department);
+                }
+                Long employeeId = rs.getLong("employee_id");
+                if (employeeId > 0) {
+                    department.getEmployees().add(Employee.builder()
+                            .id(employeeId)
+                            .fullName(rs.getString("full_name"))
+                            .dateOfBirth(rs.getDate("date_of_birth"))
+                            .phoneNumber(rs.getString("phone_number"))
+                            .emailAddress(rs.getString("email_address"))
+                            .position(rs.getString("position"))
+                            .dateOfEmployment(rs.getDate("date_of_employment"))
+                            .build());
+                }
+            }
+            return new ArrayList<>(map.values());
+        };
+    }
+
     @Override
-    public List<Department> findAll() {
-        return null;
+    public List<Department> findAllDepartmentsWithTheirUsers() {
+        return namedParameterJdbcTemplate.query(SQL_SELECT_DEPARTMENTS, resultSetExtractor());
     }
 }
--- src/main/java/com/dao/jdbc/JdbcEmployeeDaoImpl.java
+++ src/main/java/com/dao/jdbc/JdbcEmployeeDaoImpl.java
@@ -3,27 +3,44 @@ package com.dao.jdbc;
 import com.dao.EmployeeDao;
 import com.model.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 @Repository
 public class JdbcEmployeeDaoImpl implements EmployeeDao {
 
     private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
-    private final static String SQL_SELECT_EMPLOYEES = "SELECT id, full_name, " +
-            "date_of_birth, phone_number, email_address, position, date_of_employment";
+    private final static String SQL_SELECT_EMPLOYEE = "SELECT id, full_name, " +
+            "date_of_birth, phone_number, email_address, position, date_of_employment " +
+            "FROM employees WHERE id = :id";
 
     @Autowired
     public JdbcEmployeeDaoImpl(DataSource dataSource) {
         this.namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
     }
 
+    private RowMapper<Employee> rowMapper() {
+        return (rs, i) -> Employee.builder()
+                .id(rs.getLong("id"))
+                .fullName(rs.getString("full_name)"))
+                .dateOfBirth(rs.getDate("date_of_birth"))
+                .phoneNumber(rs.getString("phone_number"))
+                .emailAddress(rs.getString("email_address"))
+                .position(rs.getString("position"))
+                .dateOfEmployment(rs.getDate("date_of_employment"))
+                .build();
+    }
+
     @Override
-    public List<Employee> findAll() {
-        return null;
+    public Employee getOneEmployeeById(Long id) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("id", id);
+        return namedParameterJdbcTemplate.queryForObject(SQL_SELECT_EMPLOYEE, params, rowMapper());
     }
 }
